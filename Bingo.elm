d13e040b5b33b42a7ca81ef9ecf887bba4441776
themarkappleby <themarkappleby@gmail.com>
2016-12-11 12:01:34 -0500

    random number generation

--- Bingo.elm
+++ Bingo.elm
@@ -2,9 +2,90 @@ module Bingo exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Random
 
 
--- elm live Bingo.elm --output=bingo.js
+-- MODEL
+
+
+type alias Model =
+    { name : String
+    , gameNumber : Int
+    , entries : List Entry
+    }
+
+
+type alias Entry =
+    { id : Int
+    , phrase : String
+    , points : Int
+    , marked : Bool
+    }
+
+
+initialModel : Model
+initialModel =
+    { name = "Mark"
+    , gameNumber = 1
+    , entries = initialEntries
+    }
+
+
+initialEntries : List Entry
+initialEntries =
+    [ Entry 1 "Stuff" 100 False
+    , Entry 2 "Things" 200 False
+    , Entry 3 "Foo" 300 False
+    , Entry 4 "Bar" 400 False
+    ]
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NewGame
+    | Mark Int
+    | NewRandom Int
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NewRandom randomNumber ->
+            ( { model | gameNumber = randomNumber }, Cmd.none )
+
+        NewGame ->
+            ( { model
+                | entries = initialEntries
+              }
+            , generateRandomNumber
+            )
+
+        Mark id ->
+            let
+                markEntry entry =
+                    if entry.id == id then
+                        { entry | marked = (not entry.marked) }
+                    else
+                        entry
+            in
+                ( { model | entries = List.map markEntry model.entries }, Cmd.none )
+
+
+
+-- COMMANDS
+
+
+generateRandomNumber : Cmd Msg
+generateRandomNumber =
+    Random.generate NewRandom (Random.int 1 100)
+
+
+
+-- VIEW
 
 
 playerInfo : String -> Int -> String
@@ -12,7 +93,7 @@ playerInfo name gameNumber =
     name ++ " - Lesson #" ++ (toString gameNumber)
 
 
-viewPlayer : String -> Int -> Html msg
+viewPlayer : String -> Int -> Html Msg
 viewPlayer name gameNumber =
     let
         playerInfoText =
@@ -24,27 +105,67 @@ viewPlayer name gameNumber =
             [ playerInfoText ]
 
 
-viewHeader : String -> Html msg
+viewHeader : String -> Html Msg
 viewHeader title =
     header []
         [ h1 [] [ text title ] ]
 
 
-viewFooter : Html msg
+viewFooter : Html Msg
 viewFooter =
     footer []
         [ a [ href "http://google.com" ] [ text "Powered by Elm" ] ]
 
 
-view : Html msg
-view =
+viewEntryList : List Entry -> Html Msg
+viewEntryList entries =
+    ul [] (List.map viewEntry entries)
+
+
+viewEntry : Entry -> Html Msg
+viewEntry entry =
+    li [ classList [ ( "marked", entry.marked ) ], onClick (Mark entry.id) ]
+        [ span [ class "phrase" ] [ text entry.phrase ]
+        , span [ class "points" ] [ text (toString entry.points) ]
+        ]
+
+
+sumMarkedPoints : List Entry -> Int
+sumMarkedPoints entries =
+    entries
+        |> List.filter .marked
+        |> List.map .points
+        |> List.sum
+
+
+viewScore : Int -> Html Msg
+viewScore sum =
+    div
+        [ class "score" ]
+        [ span [ class "label" ] [ text "Score" ]
+        , span [ class "value" ] [ text (toString sum) ]
+        ]
+
+
+view : Model -> Html Msg
+view model =
     div [ class "content" ]
-        [ viewHeader "Elm Tutorial"
-        , viewPlayer "Mark" 7
+        [ viewHeader "Buzzword Bingo"
+        , viewPlayer model.name model.gameNumber
+        , viewEntryList model.entries
+        , viewScore (sumMarkedPoints model.entries)
+        , div [ class "button-group" ]
+            [ button [ onClick NewGame ] [ text "New Game" ] ]
+        , div [ class "debug" ] [ text (toString model) ]
         , viewFooter
         ]
 
 
-main : Html msg
+main : Program Never Model Msg
 main =
-    view
+    Html.program
+        { init = ( initialModel, generateRandomNumber )
+        , view = view
+        , update = update
+        , subscriptions = (\_ -> Sub.none)
+        }
